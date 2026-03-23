bcb626329b15f98cd037f30bbfc8156ac8a61a18
Adon Omeri <adon.omeri@student.education.wa.edu.au>
2026-03-23 14:45:05 +0800

    fix formatting

--- Sources/Luminare/Components/Composes/LuminareButtonCompose.swift
+++ Sources/Luminare/Components/Composes/LuminareButtonCompose.swift
@@ -20,10 +20,10 @@ public enum PositionInList {
 
 public struct LuminareButtonCompose: View {
     @Environment(\.luminareButtonComposeSpacing) private var spacing
-    
+
     private let buttons: [any View]
     private let positionInList: PositionInList
-    
+
     public init(
         _ positionInList: PositionInList = .unknown,
         @LuminareButtonBuilder _ buttons: () -> [any View]
@@ -31,22 +31,22 @@ public struct LuminareButtonCompose: View {
         self.positionInList = positionInList
         self.buttons = buttons()
     }
-    
+
     public var body: some View {
         HStack(spacing: spacing) {
             ForEach(Array(buttons.enumerated()), id: \.offset) { index, button in
                 let isFirst = index == 0
                 let isLast = index == buttons.count - 1
 
-                let roundTop    = positionInList == .unknown || positionInList == .top
+                let roundTop = positionInList == .unknown || positionInList == .top
                 let roundBottom = positionInList == .unknown || positionInList == .bottom
 
                 AnyView(
                     button
                         .luminareRoundingBehavior(
-                            topLeading:     roundTop    && isFirst,
-                            topTrailing:    roundTop    && isLast,
-                            bottomLeading:  roundBottom && isFirst,
+                            topLeading: roundTop && isFirst,
+                            topTrailing: roundTop && isLast,
+                            bottomLeading: roundBottom && isFirst,
                             bottomTrailing: roundBottom && isLast
                         )
                 )
@@ -64,20 +64,20 @@ public struct LuminareButtonCompose: View {
     LuminarePane {
         LuminareSection {
             Text("LuminareButtonCompose")
-            
+
             LuminareButtonCompose(.bottom) {
                 Button {
                     print(1)
                 } label: {
                     Text("Button 1")
                 }
-                
+
                 Button {
                     print(1)
                 } label: {
                     Text("Button 1")
                 }
-                
+
                 Button {
                     print(2)
                 } label: {
